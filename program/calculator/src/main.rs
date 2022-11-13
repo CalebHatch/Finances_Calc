be8527a110f81a5c5a48aff8c76d9da327cc7681
CalebHatch <calebhatchcontact@gmail.com>
2022-11-12 22:03:48 -0700

    Update main.rs

--- program/calculator/src/main.rs
+++ program/calculator/src/main.rs
@@ -1,13 +1,14 @@
 #![allow(unused)]
 
 use std::collections::HashMap;
+use std::hash::Hash;
 use std::io;
 use std::fs::File;
 use std::io::{Write, Read, BufReader, BufRead, ErrorKind};
 use std::cmp::Ordering;
 
-fn get_expenses(rent: i32, groceries: i32) -> i32{
-    return rent + groceries
+fn get_expenses(rent: i32, groceries: i32, restaurants: i32) -> i32{
+    return rent + groceries + restaurants
 }
 
 fn main() {
@@ -39,10 +40,21 @@ fn main() {
 
         let groceries_int = groceries.trim().parse::<i32>().unwrap();
 
-        let mut expenses = HashMap::new();
-        expenses.insert(rent_int, groceries_int);
+        println!("How much do you spend on eating out per month?");
+        let mut restaurants = String::new();
+        io::stdin()
+            .read_line(&mut restaurants)
+            .expect("Didn't receive input");
 
-        println!("Total expenses: {}", get_expenses(rent_int, groceries_int));
+        let rest_int = restaurants.trim().parse::<i32>().unwrap();
+
+        let mut rent_map = HashMap::new();
+        rent_map.insert("Rent Total", rent_int);
+
+        let mut groceries_map = HashMap::new();
+        groceries_map.insert("Groceries Total", groceries_int);
+
+        println!("Total expenses: {}", get_expenses(rent_int, groceries_int, rest_int));
 
         println!("Would you like to redo? (y/n)");
         let mut user_redo = String::new();
